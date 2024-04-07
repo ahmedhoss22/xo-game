27e6976908d378b4f939bbaaa79fba7f8da62e9e
Ahmed Hossam <ahmed>
2024-04-08 01:52:59 +0200

    otp fix issues

--- src/app/forgetpassword/otp/page.jsx
+++ src/app/forgetpassword/otp/page.jsx
@@ -1,21 +1,24 @@
 'use client'
 import '../forgetPassword.scss'
 import React from 'react'
-import  { useEffect ,useState} from "react";
+import { useEffect, useState } from "react";
 import { FaLongArrowAltLeft, FaUnlockAlt } from 'react-icons/fa';
 import { useTranslation } from "react-i18next";
 import Link from 'next/link'
 import TextField from "@mui/material/TextField";
-import icon from "@/assets/photos/icon.png"; 
+import icon from "@/assets/photos/icon.png";
 import * as Yup from "yup";
 import { useFormik } from "formik";
 import Api from '@/config/api';
 import { notifyError } from '@/components/toastify/toastify';
+import { useRouter } from 'next/navigation';
 
- const otp = () => {
-  const { t, i18n } = useTranslation(); 
+const otp = () => {
+  
+  const { t, i18n } = useTranslation();
+  const router = useRouter()
   const [loadingBtn, setLoadingBtn] = useState(false);
- 
+
   const initialValues = {
     otp1: '',
     otp2: '',
@@ -32,21 +35,22 @@ import { notifyError } from '@/components/toastify/toastify';
 
   const handleSubmit = async (values, { setSubmitting, resetForm }) => {
     const otp = `${values.otp1}${values.otp2}${values.otp3}${values.otp4}`;
-    Api.post("/users/otp-verify",{ otp })
-    .then(()=>{
-      router.push("/forgetpassword/forgetpasswordchange")
-    })
-    .catch((err)=>{
-      setLoadingBtn(false);
+    Api.post("/users/otp-verify", { otp })
+      .then(() => {
+        console.log("Otp done !!");
+        router.push("/forgetpassword/forgetpasswordchange")
+      })
+      .catch((err) => {
+        setLoadingBtn(false);
 
-      let errMsg = err?.response?.data?.message
-      notifyError(Array.isArray(errMsg)? errMsg[0]:errMsg )
-      console.log(err);
-    })
-    .finally(() => {
-      setLoadingBtn(false);
-    });
-     
+        let errMsg = err?.response?.data?.message
+        notifyError(Array.isArray(errMsg) ? errMsg[0] : errMsg)
+        console.log(err);
+      })
+      .finally(() => {
+        setLoadingBtn(false);
+      });
+
     setSubmitting(false);
   };
 
@@ -59,88 +63,88 @@ import { notifyError } from '@/components/toastify/toastify';
   return (
 
     <div className="vh-100 forget-password-bg">
-    <div className="container">
-      <div className="mb-10 mt-10">
-        <div className="d-flex fs-2 position-fixed">
-          <Link href="/forgetpassword" className="link text-white">
-            <FaLongArrowAltLeft className="" />
-          </Link>
+      <div className="container">
+        <div className="mb-10 mt-10">
+          <div className="d-flex fs-2 position-fixed">
+            <Link href="/forgetpassword" className="link text-white">
+              <FaLongArrowAltLeft className="" />
+            </Link>
+          </div>
+          <div className="d-flex justify-content-center align-items-center">
+            <h1 className="text-center fw-bold"> {t('otp.verification')}</h1>
+          </div>
         </div>
-        <div className="d-flex justify-content-center align-items-center">
-          <h1 className="text-center fw-bold"> {t('otp.verification')}</h1>
-        </div>
-      </div>
-      <form onSubmit={formik.handleSubmit}>
-        <div className="d-flex flex-col align-items-center">
-          <div className="row max-w-screen-sm">
-            <div className="col-12  mb-5">
-              <div className="col-12 d-flex justify-content-center align-items-center flex-col">
-                <div className="d-flex justify-content-center align-items-center rounded-full circle-bg">
-                  <FaUnlockAlt className="fs-2" />
-                </div>
-                <p className="mt-5 mb-3 text-center">{t('otp.title')} Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptates cum mollitia repudiandae laborum alias impedit incidunt quo ipsa. Magnam rem iusto praesentium, numquam labore ea omnis sapiente corporis et.</p>
-                <div className="flex flex-row items-center justify-between mx-auto w-full max-w-xs">
-                  <div className="w-10 h-16 ">
-                    <input
-                      maxLength="1"
-                      className="text-gray-900  h-full flex flex-col items-center justify-center text-center  outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
-                      type="text"
-                      style={{ width: '70px' }}
-                      name="otp1"
-                      value={formik.values.otp1}
-                      onChange={formik.handleChange}
-                    ></input>
+        <form onSubmit={formik.handleSubmit}>
+          <div className="d-flex flex-col align-items-center">
+            <div className="row max-w-screen-sm">
+              <div className="col-12  mb-5">
+                <div className="col-12 d-flex justify-content-center align-items-center flex-col">
+                  <div className="d-flex justify-content-center align-items-center rounded-full circle-bg">
+                    <FaUnlockAlt className="fs-2" />
                   </div>
-                  <div className="w-10 h-16 ">
-                    <input
-                      maxLength="1"
-                      className="text-gray-900  h-full flex flex-col items-center justify-center text-center  outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
-                      type="text"
-                      style={{ width: '70px' }}
-                      name="otp2"
-                      value={formik.values.otp2}
-                      onChange={formik.handleChange}
-                    ></input>
-                  </div>
-                  <div className="w-10 h-16 ">
-                    <input
-                      maxLength="1"
-                      className="text-gray-900   h-full flex flex-col items-center justify-center text-center  outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
-                      type="text"
-                      style={{ width: '70px' }}
-                      name="otp3"
-                      value={formik.values.otp3}
-                      onChange={formik.handleChange}
-                    ></input>
-                  </div>
-                  <div className="w-10 h-16 ">
-                    <input
-                      maxLength="1"
-                      className="text-gray-900   h-full flex flex-col items-center justify-center text-center  outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
-                      type="text"
-                      style={{ width: '70px' }}
-                      name="otp4"
-                      value={formik.values.otp4}
-                      onChange={formik.handleChange}
-                    ></input>
+                  <p className="mt-5 mb-3 text-center">{t('otp.title')} Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptates cum mollitia repudiandae laborum alias impedit incidunt quo ipsa. Magnam rem iusto praesentium, numquam labore ea omnis sapiente corporis et.</p>
+                  <div className="flex flex-row items-center justify-between mx-auto w-full max-w-xs">
+                    <div className="w-10 h-16 ">
+                      <input
+                        maxLength="1"
+                        className="text-gray-900  h-full flex flex-col items-center justify-center text-center  outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                        type="text"
+                        style={{ width: '70px' }}
+                        name="otp1"
+                        value={formik.values.otp1}
+                        onChange={formik.handleChange}
+                      ></input>
+                    </div>
+                    <div className="w-10 h-16 ">
+                      <input
+                        maxLength="1"
+                        className="text-gray-900  h-full flex flex-col items-center justify-center text-center  outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                        type="text"
+                        style={{ width: '70px' }}
+                        name="otp2"
+                        value={formik.values.otp2}
+                        onChange={formik.handleChange}
+                      ></input>
+                    </div>
+                    <div className="w-10 h-16 ">
+                      <input
+                        maxLength="1"
+                        className="text-gray-900   h-full flex flex-col items-center justify-center text-center  outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                        type="text"
+                        style={{ width: '70px' }}
+                        name="otp3"
+                        value={formik.values.otp3}
+                        onChange={formik.handleChange}
+                      ></input>
+                    </div>
+                    <div className="w-10 h-16 ">
+                      <input
+                        maxLength="1"
+                        className="text-gray-900   h-full flex flex-col items-center justify-center text-center  outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+                        type="text"
+                        style={{ width: '70px' }}
+                        name="otp4"
+                        value={formik.values.otp4}
+                        onChange={formik.handleChange}
+                      ></input>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
+            <div className="col-md-3 col-8 ms-4">
+              <button
+                type="submit"
+                className="forget-btn focus:ring-4  focus:outline-none font-medium rounded-lg text-sm px-36 py-3 text-center"
+              >
+                {t('otp.verify')}
+              </button>
+            </div>
           </div>
-          <div className="col-md-3 col-8 ms-4">
-            <button
-              type="submit"
-              className="forget-btn focus:ring-4  focus:outline-none font-medium rounded-lg text-sm px-36 py-3 text-center"
-            >
-              {t('otp.verify')}
-            </button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
-  
-   )
+
+  )
 }
 export default otp
